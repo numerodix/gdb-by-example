08f4e758c3495ba46054c03b1d8c94c9101dfeb7
Martin Matusiak <numerodix@gmail.com>
2019-11-28 19:49:37 +1100

    more table 4

--- c-primitive/main.cpp
+++ c-primitive/main.cpp
@@ -11,8 +11,13 @@ int main(int argc, char **argv) {
     char *pinitial = &initial;
     char *pfirst = &first[0];
     char *plast = &last[0];
+ 
+    // village population 243
+    unsigned char village = 243;
+    unsigned char *pvillage = &village;
+    unsigned char villages[] = {243, 113, 54};
+    unsigned char *pvillages = &villages[0];
 
-    unsigned char village = 240;
     unsigned short town = 63 * 1000;
     unsigned int city = 12 * 1000 * 1000;
     unsigned long long universe = 5LL << 60;
